481388a5f178870acc80387ecf0b42566a9d4184
jasmin <jasminchakma895@gmail.com>
2023-11-06 16:51:45 +0600

    request modal done

--- src/Components/FoodDetails/FoodDetails.jsx
+++ src/Components/FoodDetails/FoodDetails.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Modal from "../Modal/Modal";
 
 const FoodDetails = () => {
   const food = useLoaderData();
@@ -11,15 +12,20 @@ const FoodDetails = () => {
     expiredDateTime,
     additionalNotes,
   } = food;
+
   return (
-    <div className="my-20 flex items-center justify-around gap-10">
+    <div className="my-20 md:flex items-center justify-around gap-10">
       <div>
-        <img src={foodImage} alt="" className="rounded-lg md:w-[500px] md:h-auto" />
+        <img
+          src={foodImage}
+          alt=""
+          className="rounded-lg md:w-[500px] md:h-auto"
+        />
       </div>
       <div>
         <p className="font-bold text-2xl">{foodName}</p>
-        <p>Quantity : {foodQuantity}</p>
         <p>{additionalNotes}</p>
+        <p>Quantity : {foodQuantity}</p>
         <p>Expire Date : {expiredDateTime}</p>
         <p>Food PickUp Address : {pickupLocation}</p>
         <p className="text-base font-bold">Donner information : </p>
@@ -30,6 +36,15 @@ const FoodDetails = () => {
             alt=""
             className=" mt-5 w-72 ring-sky-700 ring-2 outline-offset-1"
           />
+        </div>
+        <div className="mt-10">
+          <button
+            onClick={() => document.getElementById("my_modal_1").showModal()}
+            className="bg-sky-700 text-white p-2"
+          >
+            Request for pickup
+          </button>
+          <Modal food = {food}></Modal>
         </div>
       </div>
     </div>
---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,159 @@
+import moment from "moment/moment";
+import UseAuth from "../../Hooks/UseAuth";
+import PropsTypes from "prop-types";
+
+const Modal = ({ food }) => {
+  const { user } = UseAuth();
+  console.log(user);
+  const {
+    _id,
+    foodImage,
+    foodName,
+    donator,
+    pickupLocation,
+    expiredDateTime,
+    additionalNotes,
+  } = food;
+
+  const currentTime = moment().format(`h:mm A`);
+  console.log(currentTime);
+  return (
+    <div className="">
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box relative">
+          <div className="w-full">
+            <form className="card-body">
+              <p className="font-bold text-xl text-center">{foodName}</p>
+              <div className="flex justify-center">
+                <img src={foodImage} alt="" className="w-60 h-60 rounded" />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Food Id</span>
+                </label>
+                <input
+                  type="text"
+                  name="foodId"
+                  defaultValue={_id}
+                  placeholder="Full Name"
+                  className="input input-bordered"
+                  readOnly
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  defaultValue={user.displayName}
+                  placeholder="Full Name"
+                  className="input input-bordered"
+                  readOnly
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your email</span>
+                </label>
+                <input
+                  type="email"
+                  name="your email"
+                  defaultValue={user.email}
+                  className="input input-bordered"
+                  readOnly
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Donator Name</span>
+                </label>
+                <input
+                  name="donator"
+                  defaultValue={donator.name}
+                  className="input input-bordered"
+                  readOnly
+                />
+              </div>
+              <div className="form-control ">
+                <label className="label">
+                  <span className="label-text">Request Time</span>
+                </label>
+                <input
+                  type="text"
+                  name="time"
+                  defaultValue={currentTime}
+                  className="input input-bordered"
+                  readOnly
+                />
+              </div>
+              <div className="form-control ">
+                <label className="label">
+                  <span className="label-text">Pickup Location</span>
+                </label>
+                <input
+                  type="text"
+                  name="pickuplocation"
+                  defaultValue={pickupLocation}
+                  className="input input-bordered"
+                  readOnly
+                />
+              </div>
+              <div className="form-control ">
+                <label className="label">
+                  <span className="label-text">Expire Date</span>
+                </label>
+                <input
+                  type="text"
+                  name="expiredate"
+                  defaultValue={expiredDateTime}
+                  className="input input-bordered"
+                  readOnly
+                />
+              </div>
+              <div className="form-control ">
+                <label className="label">
+                  <span className="label-text">Donation money</span>
+                </label>
+                <input
+                  type="number"
+                  name="donation"
+                  placeholder="$ Donation"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control ">
+                <label className="label">
+                  <span className="label-text">Additional Notes</span>
+                </label>
+                <textarea
+                  type="number"
+                  name="donation"
+                  defaultValue={additionalNotes}
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="p-3 bg-sky-700 rounded-lg text-white">
+                  Request
+                </button>
+              </div>
+            </form>
+            <div className="modal-action">
+              <form method="dialog">
+                <button className="btn"> close </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  food: PropsTypes.object,
+};
+
+export default Modal;
